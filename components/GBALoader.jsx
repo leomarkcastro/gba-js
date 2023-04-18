9ea3e0dd1e100d9b82faee18b89b72d1f8257da2
Leo Mark Castro <66856556+leomarkcastro@users.noreply.github.com>
2023-04-18 17:20:53 +0800

    added mobile controller

--- components/GBALoader.jsx
+++ components/GBALoader.jsx
@@ -2,6 +2,7 @@ import pb from "@/lib/pocketbase";
 import GameBoyAdvance from "gbajs";
 import { useEffect, useRef, useState } from "react";
 import { useRunOnce } from "./hooks/useRunOnce";
+import { m } from "framer-motion";
 
 export default function GBALoader({ record }) {
   const recordParsed = JSON.parse(record);
@@ -340,6 +341,12 @@ export default function GBALoader({ record }) {
     setSaveLoadStatus(0);
   }
 
+  function onKeyInteraction(keyCode, isDown = true) {
+    const keypad = gba.current.keypad;
+    if (isDown) keypad.keydown(keypad[keyCode]);
+    else keypad.keyup(keypad[keyCode]);
+  }
+
   useEffect(() => {
     if (!canvasRef.current) return;
     const load = setTimeout(async () => {
@@ -357,7 +364,7 @@ export default function GBALoader({ record }) {
   });
 
   return (
-    <>
+    <div className="flex-col block ">
       <div className="p-2">
         <p className="text-center animate-pulse">
           {saveLoadStatus == 1 && "🟢 Loading Save From Internet"}
@@ -366,11 +373,11 @@ export default function GBALoader({ record }) {
       </div>
       <div
         ref={parentCanvasRef}
-        className="mx-auto border border-black shadow-md"
+        className="flex-1 mx-auto border border-black shadow-md"
         style={{
           maxWidth: "100vw",
           aspectRatio: "3/2",
-          maxHeight: "60vh",
+          maxHeight: "70vh",
         }}
       >
         <canvas
@@ -382,7 +389,7 @@ export default function GBALoader({ record }) {
         ></canvas>
       </div>
 
-      <div className="flex gap-2 m-4 mx-auto w-fit">
+      <div className="flex gap-2 m-2 mx-auto w-fit">
         {!gameRunning ? (
           <>
             <button className="d-btn d-btn-primary" onClick={startGame}>
@@ -391,12 +398,163 @@ export default function GBALoader({ record }) {
           </>
         ) : (
           <>
-            <button className="d-btn d-btn-primary" onClick={pauseUnpauseGame}>
+            <button
+              className="d-btn d-btn-primary d-btn-sm"
+              onClick={pauseUnpauseGame}
+            >
               Pause / Unpase
             </button>
           </>
         )}
       </div>
-    </>
+
+      <svg
+        width="320"
+        height="265"
+        viewBox="0 0 320 265"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        className="flex w-full h-[37vh] md:flex-0 md:hidden"
+        preserveAspectRatio="xMidYMid meet"
+      >
+        <rect width="320" height="265" fill="#b6b6b6" />
+        {/* A */}
+        <circle
+          cx="258"
+          cy="42"
+          r="25"
+          fill="#0d9b2e"
+          onPointerDown={() => {
+            onKeyInteraction("A");
+          }}
+          onPointerUp={() => {
+            onKeyInteraction("A", false);
+          }}
+        />
+        {/* B */}
+        <circle
+          cx="225"
+          cy="102"
+          r="25"
+          fill="#122699"
+          onPointerDown={() => {
+            onKeyInteraction("B");
+          }}
+          onPointerUp={() => {
+            onKeyInteraction("B", false);
+          }}
+        />
+        {/* L */}
+        <circle
+          cx="225"
+          cy="164"
+          r="25"
+          fill="#158fa1"
+          onPointerDown={() => {
+            onKeyInteraction("L");
+          }}
+          onPointerUp={() => {
+            onKeyInteraction("L", false);
+          }}
+        />
+        {/* R */}
+        <circle
+          cx="258"
+          cy="224"
+          r="25"
+          fill="#94159d"
+          onPointerDown={() => {
+            onKeyInteraction("R");
+          }}
+          onPointerUp={() => {
+            onKeyInteraction("R", false);
+          }}
+        />
+        {/* up */}
+        <rect
+          x="70"
+          y="32"
+          width="40"
+          height="50"
+          fill="#7f522a"
+          onPointerDown={() => {
+            onKeyInteraction("UP");
+          }}
+          onPointerUp={() => {
+            onKeyInteraction("UP", false);
+          }}
+        />
+        {/* down */}
+        <rect
+          x="70"
+          y="122"
+          width="40"
+          height="50"
+          fill="#440c0c"
+          onPointerDown={() => {
+            onKeyInteraction("DOWN");
+          }}
+          onPointerUp={() => {
+            onKeyInteraction("DOWN", false);
+          }}
+        />
+        {/* left */}
+        <rect
+          x="20"
+          y="82"
+          width="50"
+          height="40"
+          fill="#1e621e"
+          onPointerDown={() => {
+            onKeyInteraction("LEFT");
+          }}
+          onPointerUp={() => {
+            onKeyInteraction("LEFT", false);
+          }}
+        />
+        {/* right */}
+        <rect
+          x="110"
+          y="82"
+          width="50"
+          height="40"
+          fill="#19246f"
+          onPointerDown={() => {
+            onKeyInteraction("RIGHT");
+          }}
+          onPointerUp={() => {
+            onKeyInteraction("RIGHT", false);
+          }}
+        />
+        {/* start */}
+        <rect
+          x="110"
+          y="199"
+          width="50"
+          height="40"
+          fill="#651562"
+          onPointerDown={() => {
+            onKeyInteraction("START");
+          }}
+          onPointerUp={() => {
+            onKeyInteraction("START", false);
+          }}
+        />
+        {/* select */}
+        <rect
+          x="20"
+          y="199"
+          width="50"
+          height="40"
+          fill="#8d1b3a"
+          onPointerDown={() => {
+            onKeyInteraction("SELECT");
+          }}
+          onPointerUp={() => {
+            onKeyInteraction("SELECT", false);
+          }}
+        />
+      </svg>
+    </div>
   );
 }
